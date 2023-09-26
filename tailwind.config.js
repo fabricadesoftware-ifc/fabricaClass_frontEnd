cbda94849c6abaef9d8fc9b7346279979d5f4846
Wecreds <Wecredsbr@gmail.com>
2023-09-26 14:40:49 -0300

    feat(global): :lipstick: Font-Awesome para ícones/emojis & Inter como fonte de texto.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,tsx,jsx,ts}"],
   theme: {
     extend: {},
+    fontFamily: {
+      Inter: ["Inter, sans-serif"],
+    }
   },
   plugins: [],
 }
