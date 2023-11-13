ca7de57f337c86e12b82542c0a40631b4fcb79a5
iTzMvz <matheus.vidal.zenere@gmail.com>
2023-11-13 15:41:07 -0300

    feat: Criação dos formularios (parte dos alunos)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,27 @@
 <script setup>
+import { ref } from 'vue'
 
+const listaCards =
+  ref([
+    {
+      data: '6 de novembro de 2023',
+      aula: 'Aula X, if else',
+      horarioInicio: '9:45',
+      horarioFim: '11:15'
+    },
+    {
+      data: '6 de novembro de 2023',
+      aula: 'Aula X, if else',
+      horarioInicio: '9:45',
+      horarioFim: '11:15'
+    },
+    {
+      data: '6 de novembro de 2023',
+      aula: 'Aula X, if else',
+      horarioInicio: '9:45',
+      horarioFim: '11:15'
+    }
+  ])
 </script>
 
 <template>
