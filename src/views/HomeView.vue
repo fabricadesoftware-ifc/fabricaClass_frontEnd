1a79d18ac2d3f17c4e67c0f1475d934dd23349e9
iTzMvz <matheus.vidal.zenere@gmail.com>
2023-11-14 15:16:07 -0300

    update: Implementação dos icones nos formularios

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,27 @@
 <script setup>
 import { ref } from 'vue'
+import FileDocumentOutline from 'vue-material-design-icons/FileDocumentOutline.vue'
 
 const listaCards =
   ref([
+    {
+      data: '6 de novembro de 2023',
+      aula: 'Aula X, if else',
+      horarioInicio: '9:45',
+      horarioFim: '11:15'
+    },
+    {
+      data: '6 de novembro de 2023',
+      aula: 'Aula X, if else',
+      horarioInicio: '9:45',
+      horarioFim: '11:15'
+    },
+    {
+      data: '6 de novembro de 2023',
+      aula: 'Aula X, if else',
+      horarioInicio: '9:45',
+      horarioFim: '11:15'
+    },
     {
       data: '6 de novembro de 2023',
       aula: 'Aula X, if else',
