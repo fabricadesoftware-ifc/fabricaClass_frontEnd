1eec9a488f4691f602f64df83155b36c50622b90
JSkahl <josekahl333@gmail.com>
2023-12-11 10:32:36 -0300

    feat: criação da header

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <nav class="h-20 ma-w-max shadow-lg flex justify-between">
+        <img src="../assets/logo-fabrica.png" class="ml-40">
+        
+        <div class="w-32 h-10 flex justify-between mr-40 mt-5">
+            <i class="fa-solid fa-user text-4xl"></i>
+            <i class="fa-solid fa-bars text-4xl"></i>
+        </div>
+    </nav>
+</template>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,7 @@
-<template>
-  <div>
-
-  </div>
-</template>
-
 <script setup>
-
+  import Header from '../components/Header.vue';
 </script>
 
+<template>
+  <Header/>
+</template>
