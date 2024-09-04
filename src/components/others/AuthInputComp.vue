2ee7997d2c5c91de0bfe614d794a188cf7f10042
vinyzada <vinyescolas@gmail.com>
2024-09-04 11:04:32 -0300

    refactor: reduced style on AuthInputComp #29

--- src/components/others/AuthInputComp.vue
+++ src/components/others/AuthInputComp.vue
@@ -33,57 +33,35 @@ const inputValue = defineModel()
 <style scoped>
 .inputEmail::before {
   content: 'email';
-  width: 60px;
-  display: flex;
-  align-items: center;
-  text-align: center;
-  height: 15px;
-  display: block;
-  position: absolute;
-  background-color: white;
-  transform: translate(15px, -10px);
+  width: 55px;
 }
 .inputPassword::before {
   content: 'senha';
   width: 60px;
-  display: flex;
-  align-items: center;
-  text-align: center;
-  height: 15px;
-  display: block;
-  position: absolute;
-  background-color: white;
-  transform: translate(15px, -10px);
 }
 .inputConfirmPassword::before {
   content: 'Confirme a senha';
   width: 150px;
-  display: flex;
-  align-items: center;
-  text-align: center;
-  height: 15px;
-  display: block;
-  position: absolute;
-  background-color: white;
-  transform: translate(15px, -10px);
 }
 .inputMatricula::before {
   content: 'matricula';
   width: 90px;
-  display: flex;
-  align-items: center;
-  text-align: center;
-  height: 15px;
-  display: block;
-  position: absolute;
-  background-color: white;
-  transform: translate(15px, -10px);
 }
 .inputBase{
   border: 1px solid black;
   height: 65px;
   border-radius: 0.75rem;
   z-index: 2;
+}
+.inputBase::before{
+  display: flex;
+  align-items: center;
+  text-align: center;
+  height: 15px;
+  display: block;
+  position: absolute;
+  background-color: white;
+  transform: translate(15px, -10px);
 }
 input[type="password"]::-ms-reveal {
     display: none;
